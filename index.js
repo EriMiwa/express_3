942a5421b2a65bf5209c2b14295ca0c89e6a1284
Erika Miwa <erikamiwa612@gmail.com>
2019-09-11 10:03:09 -0700

    adjust models and routes

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
 const express  = require('express');
-const path =  require('path');
+// const path =  require('path');
 const recipes = require('./models/recipes.js');
 const uuid = require('uuid/v1');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.get('/api/recipes', (req, res) => {
-    // res.send("Recipe List")
-    res.json(recipes);
+// const ATLAS_URI = "mongodb+srv://mitsu_0612:ironmouse807@cluster0-htwos.mongodb.net/test?retryWrites=true&w=majority";
+const ATLAS_URI = process.env.ATLAS_URI;
+
+mongoose.connect(ATLAS_URI, {
+    useNewUrlParser: true, 
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
 
-app.post('/api/recipes', (req , res) => {
-    const { image_url, name } = req.body;
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('mongoDB connection worked!')
+})
 
-    const newRecipes = {
-        id: uuidv1(),
-        image_url: image_url,
-        name: name,
-    };
-
-    recipes.push(newRecipes);
-    res.json({ msg: "new recipe added!"});
-});
-
-app.put('/api/recipes', (req , res) => {
-    const id = req.body.id;
-    const name = req.body.name;
-    const image_url = req.body.image_url;
-
-    const index = recipes.findIndex(k => k.id == id);
-    recipes[index] = {
-        id: id,
-        // name: name ? name : recipes[index].name,
-        name: name,
-        image_url,
-    }
-    res.json(recipes);
-});
-
-app.delete('/api/recipes', (req , res) => {
-    const id = req.body.id;
-
-    const index = recipes.findIndex(r => r.id == id);
-    recipes.splice(index, 1);
-    res.json(recipes);
-});
+//Routes
+app.use('/api/recipes', require('./routes/recipes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT,() => console.log(`Server started at port ${PORT}`));
